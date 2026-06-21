251532bcf9b0968c9417d794b344ca3e29c37b06
Ava Prime <ava.prime@outlook.com>
2026-06-21 14:06:32 +0200

    Export new email and notion tools

--- ultimate_assistant/tools/__init__.py
+++ ultimate_assistant/tools/__init__.py
@@ -1,5 +1,7 @@
 from .research import research
 from .image_gen import generate_image
 from .image_analyze import analyze_image
+from .email import send_email
+from .notion import add_to_notion_database
 
-__all__ = ["research", "generate_image", "analyze_image"]
+__all__ = ["research", "generate_image", "analyze_image", "send_email", "add_to_notion_database"]
